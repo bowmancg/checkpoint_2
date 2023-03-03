3fa9a5b0406b7bbf2be285476732a6595827bf1f
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-03 12:11:41 -0700

    html skeleton and started logic

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-let 
+let runes = 0
+let total = 0
+
+const boss = {
+    name: 'Malenia',
+    health: 1000,
+    maxHealth: 1000,
+    damage: 30,
+    weaponArt: 'Waterfowl Dance'
+}
+
+const player = {
+    name: 'The Tarnished',
+    health: 100,
+    damage: 5,
+    weaponArt: '',
+    level: 1
+}
+
