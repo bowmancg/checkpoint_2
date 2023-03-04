489d8e1b3a3c6009ab7cd947dfc6577b6530e34f
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-03 17:03:59 -0700

    bunch of functions

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 let runes = 0
-let total = 0
 
 const boss = {
     name: 'Malenia',
     health: 1000,
     maxHealth: 1000,
     damage: 30,
-    weaponArt: 'Waterfowl Dance'
+    currentWeapon: 'Hand of Malenia'
 }
 
 const player = {
     name: 'The Tarnished',
     health: 100,
     damage: 10,
-    weaponArt: '',
-    level: 1
+    currentWeapon: 'Longsword',
 }
 
 let clickWeapon = [
@@ -23,6 +21,87 @@ let clickWeapon = [
         price: 100,
         damage: 50,
         quantity: 0,
-        
+    },
+    {
+        name: 'Carian Longsword',
+        price: 50,
+        damage: 30,
+        quantity: 0
     }
 ]
+
+
+function acquireWeapon(name) {
+    let currentWeapon = weapon.find(w => w.name == name)
+    if (runes >= currentWeapon.price) {
+        currentWeapon++
+        runes -= currentWeapon.price
+        drawPlayerStats()
+    }
+}
+
+function playerAttacksBoss() {
+    let tarnishedDamage = player.damage
+    if (boss.maxHealth <= 1000) {
+        boss.maxHealth -= tarnishedDamage
+    }
+    drawBossStats()
+}
+
+function acquireRunes() {
+    if (boss.maxHealth != 1000) 
+        runes += 30
+    playerAttacksBoss()
+    drawRunes()
+}
+
+function bossAttacksPlayer() {
+    setInterval(() => {
+        player.health -= boss.damage
+        drawPlayerStats()
+    }, 3000)
+}
+
+function drawPlayerStats() {
+    let runeElem = document.getElementById('runes')
+    let playerHealthElem = document.getElementById('playerHealth-elem')
+    let playerDamageElem = document.getElementById('playerDamage-elem')
+    if (player.health <= 0) {
+        stopInterval()
+        setTimeout(() => {
+            window.alert('You Died')
+        }, 200)
+    }
+}
+
+function drawBossStats() {
+    let bossStatsTemplate = ''
+    bossStatsTemplate += `
+    <div class="card-body text-danger fs-5 text-center">
+                    <span id="health-bar">${boss.maxHealth}</span>
+                </div>
+    `
+    document.getElementById('health-bar').innerHTML = bossStatsTemplate
+    if (boss.maxHealth <= 0) {
+        stopInterval()
+        setTimeout(() => {
+            window.alert('Demigod Slain')
+        }, 200)
+    }
+}
+
+function bossHit() {
+    if (boss.maxHealth > 0) {
+        boss.maxHealth -= 10
+        runes += 25
+        drawBossStats()
+        drawPlayerStats()
+    }
+}
+
+function stopInterval() {
+    clearInterval(bossHit())
+}
+
+drawBossStats()
+drawPlayerStats()
