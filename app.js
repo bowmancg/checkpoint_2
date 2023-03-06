ca17a24fbea31b52253c087985411753a7335e50
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-05 17:32:29 -0700

    final commit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const player = {
     health: 100,
     damage: 10,
     currentWeapon: 'Longsword',
+    weaponInventory: []
 }
 
 const weapons = [
@@ -20,6 +21,7 @@ const weapons = [
         name: 'Longsword',
         price: 0,
         damage: 10,
+        image: 'https://static.miraheze.org/eldenringwiki/4/48/Longsword_Full.png'
     },
     {
         name: 'Greatsword',
@@ -34,7 +36,7 @@ const weapons = [
         image: 'https://static.miraheze.org/eldenringwiki/7/76/Carian_Knight%27s_Sword_Full.png'
     },
     {
-        name: 'Marika\'s Hammer',
+        name: 'Marikas Hammer',
         price: 180,
         damage: 150,
         image: 'https://static.miraheze.org/eldenringwiki/e/e7/Marika%27s_Hammer_Full.png'
@@ -47,20 +49,47 @@ const weapons = [
     }
 ]
 
+function drawWeapons() {
+    let template = ''
+
+
+    weapons.forEach(w => {
+        template += `
+        <div class="card bg-dark border border-secondary">
+                    <img onclick="acquireWeapon('${w.name}')" class="card-img-top" src="${w.image}" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">${w.name}</h5>
+                    </div>
+                    <div class="card-footer">${w.price} runes</div>
+                </div>
+        `
+    })
+    document.getElementById('weapon-cards').innerHTML = template
+}
 
 function acquireWeapon(weaponName) {
     let currentWeapon = weapons.find(w => w.name == weaponName)
-    if (runes >= currentWeapon.price) {
-        runes -= currentWeapon.price
-        player.currentWeapon = currentWeapon.name
-        player.damage = currentWeapon.damage
-        drawPlayerStats()
+    if (!player.weaponInventory.some(w => w.name == weaponName)) {
+        if (runes >= currentWeapon.price) {
+            runes -= currentWeapon.price
+            currentWeapon.price = Math.floor(currentWeapon.price * 1.1)
+            player.currentWeapon = currentWeapon.name
+            player.damage = currentWeapon.damage
+            player.weaponInventory.push(currentWeapon)
+            console.log(player);
+            drawPlayerStats()
+            drawInventory()
+            drawWeapons()
+        }
     }
 }
 
 function playerAttacksBoss() {
     if (boss.maxHealth <= 1000) {
         boss.maxHealth -= player.damage
+    }
+    else {
+     boss.maxHealth = 0
     }
     hitBoss()
 }
@@ -75,6 +104,25 @@ let bossAttack = setInterval(() => {
     if (player.health < 0) player.health = 0
     drawPlayerStats()
 }, 3000)
+
+let bleedInterval = null
+
+function purchaseBleedEffect(){
+    //purchase bleed for 100 runes
+    if (runes >= 100) {
+        runes -= 100
+        bleedInterval = setInterval(() => {
+            runes += 30
+            boss.maxHealth -= 30
+            if (boss.maxHealth <= 0){
+                boss.maxHealth = 0  
+                clearInterval(bleedInterval)
+            } 
+            drawBossStats()
+            drawPlayerStats()
+        }, 3000) 
+    }
+}
 
 function drawPlayerStats() {
     let runeElem = document.getElementById('runes')
@@ -93,7 +141,13 @@ function drawPlayerStats() {
     }
 }
 
-
+function drawInventory() {
+    let template = ''
+    player.weaponInventory.forEach(w => {
+        template += `${w.name} `    
+    })
+    document.getElementById('inventory').innerHTML = template
+}
 
 function drawBossStats() {
     let bossStatsTemplate = ''
@@ -123,5 +177,7 @@ function stopInterval() {
     clearInterval(bossAttack)
 }
 
+drawInventory()
+drawWeapons()
 drawBossStats()
 drawPlayerStats()
